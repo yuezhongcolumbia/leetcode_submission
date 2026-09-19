7d0ec4f564ea25977b1c37e59d28fd00f015c380
yuezhongcolumbia <167635281+yuezhongcolumbia@users.noreply.github.com>
2026-09-19 13:40:04 -0400

    Time: 0 ms (100%), Space: 43.3 MB (73.09%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -2,13 +2,26 @@ class Solution {
     public int hIndex(int[] citations) {
         
         // 0 1 2 3 4
-        // 0 1 3 5 6
-        Arrays.sort(citations);
-        int h = citations.length;
-        for(int i = 0; i < citations.length; i++){
-            if (citations[i] >= h) return h;
-            h--;
+        // 3 0 6 1 5
+       
+        // 0 1 2 3 4 5
+        // 1 1    1  2
+        int n = citations.length;
+        int[] count = new int[n + 1];
+        for(int num: citations){
+            count[Math.min(n, num)]++;
         }
-        return h;
+        int papers = 0;
+        for(int i = n; i >= 0; i--){
+            papers += count[i];
+            if (papers >= i) return i;
+        }
+        return 0;
+
+        
+
+
+
+        
     }
 }
