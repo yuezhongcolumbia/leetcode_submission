032652010b4ff6d54bfc1f230ca06bd65085c931
yuezhongcolumbia <167635281+yuezhongcolumbia@users.noreply.github.com>
2026-09-24 20:13:48 -0400

    Time: 1 ms (98.06%), Space: 46.8 MB (93.97%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,44 +1,25 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        // use start or end for finding insertion point? start
-        
-        // [1,100]   [101,102]    [99,102]
-
-
-        // insert on the left
-        //     merge with prev interval?
-        //     merge with cur interval?
-           
-        //     if I merge into 
-        if (intervals.length == 0){
-            return new int[][]{{newInterval[0], newInterval[1]}};
-        }
-        List<int[]> res = new ArrayList<>();
-        int pos = binarySearch(intervals, newInterval);
+        // 1. can merge with current
+        //     no intersect, add cur interval to result
+        //     intersection, do not add anything, update tmp, and keep merging
+        // 2. can directly insert
+        //     can insert in front：  
+        int n = intervals.length;
         int[] tmp = newInterval;
-
-        if (pos > 0){
-            for(int i = 0; i < pos - 1; i++){
+        List<int[]> res = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if (tmp == null){
                 res.add(intervals[i]);
             }
-             if (intersect(intervals[pos - 1], newInterval)){
-                tmp = merge(intervals[pos - 1], newInterval);
-             } else {
-                res.add(intervals[pos - 1]);
-             }
-        }
-       
-        for(int i = pos; i < intervals.length; i++){
-            if (intersect(intervals[i], tmp)){
-                tmp = merge(intervals[i], tmp);
-            } else {
-                res.add(tmp); 
-                for(int j = i; j < intervals.length; j++){
-                    res.add(intervals[j]);
-                }
-                
+            else if (intersect(tmp, intervals[i])){
+                tmp = merge(tmp, intervals[i]);
+            } else if (canInsertInFront(tmp, intervals[i])){
+                res.add(tmp);
+                res.add(intervals[i]);
                 tmp = null;
-                break;
+            } else {
+                res.add(intervals[i]);
             }
         }
         if (tmp != null) res.add(tmp);
@@ -47,27 +28,17 @@ class Solution {
             resArr[i] = res.get(i);
         }
         return resArr;
-        
+
+
+           
     }
     public boolean intersect(int[] a, int[] b){
-        return !(a[1] < b[0] || a[0] > b[1]) ;
+        return !(a[1] < b[0]|| a[0] > b[1]);
     }
     public int[] merge(int[] a, int[] b){
-        return new int[]{Math.min(a[0], b[0]), Math.max(a[1],b[1])};
+        return new int[]{Math.min(a[0], b[0]),  Math.max(a[1], b[1])  };
     }
-    public int binarySearch(int[][] intervals, int[] newInterval){
-        int l = 0;
-        int r = intervals.length - 1;
-        int res = -1;
-        while(l <= r){
-            int m = (l + r) >> 1;
-            if (intervals[m][0] >= newInterval[0]){
-                res = m;
-                r = m - 1;
-            } else {
-                l = m + 1;
-            }
-        }
-        return res != -1 ? res : intervals.length ;
+    public boolean canInsertInFront(int[] a, int[] b){
+        return a[1] < b[0];
     }
 }
